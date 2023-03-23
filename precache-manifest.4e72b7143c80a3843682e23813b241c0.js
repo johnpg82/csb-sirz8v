f2a46463e17e43e89655d741ea637a4c5883d17c
Code Sandbox <hello@codesandbox.io>
2023-03-23 21:29:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4e72b7143c80a3843682e23813b241c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a95f18b76d31854ec3e2131ca439344",
+    "url": "https://johnpg82.github.io/csb-sirz8v/index.html"
+  },
+  {
+    "revision": "12e0f34a60129ab4533d",
+    "url": "https://johnpg82.github.io/csb-sirz8v/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "81a7ed257788811be7de",
+    "url": "https://johnpg82.github.io/csb-sirz8v/static/js/2.c6682056.chunk.js"
+  },
+  {
+    "revision": "12e0f34a60129ab4533d",
+    "url": "https://johnpg82.github.io/csb-sirz8v/static/js/main.ba9de18a.chunk.js"
+  },
+  {
+    "revision": "15e861cfb1b5368907c6",
+    "url": "https://johnpg82.github.io/csb-sirz8v/static/js/runtime~main.5102771c.js"
+  }
+]);
